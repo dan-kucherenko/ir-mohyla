4f03346e9c419b8e7656dd38b8632eff55861f30
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-30 01:21:50 +0300

    Updated implementation for posting weight searcher

--- ir-kucherenko-7/src/main/java/kma/ir/kucherenko/posting_weight/PostingWeightSearcher.java
+++ ir-kucherenko-7/src/main/java/kma/ir/kucherenko/posting_weight/PostingWeightSearcher.java
@@ -1,14 +1,15 @@
 package kma.ir.kucherenko.posting_weight;
 
-import com.kursx.parser.fb2.Description;
-import com.kursx.parser.fb2.DocumentInfo;
 import com.kursx.parser.fb2.FictionBook;
 import com.kursx.parser.fb2.Person;
 import kma.ir.kucherenko.spimi.Spimi;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class PostingWeightSearcher {
@@ -22,29 +23,34 @@ public class PostingWeightSearcher {
         spimi.executeSPIMI();
     }
 
-    public List<PostingWeight> findDocuments(String word) {
-        String searchWord = word.toLowerCase().trim();
+    public String findDocuments(String query) {
         List<PostingWeight> result = new LinkedList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(SPIMI_RES))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] split = line.split(":");
-                if (Objects.equals(searchWord, split[0])) {
-                    String[] postings = split[1].replace('[', ' ').replace(']', ' ').split(",");
-                    for (int i = 0; i < postings.length; i++) {
-                        int bookId = Integer.parseInt(postings[i].trim());
-                        FictionBook book = new FictionBook(documents[bookId - 1]);
-                        PostingWeight postingDocument = new PostingWeight(bookId - 1, Scores.CONTENT);
-                        checkTitle(book, searchWord, postingDocument);
-                        checkAuthors(book, searchWord, postingDocument);
-                        result.add(postingDocument);
+        for (String word : query.split(" ")) {
+            String searchWord = word.toLowerCase().trim();
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(SPIMI_RES))) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    String[] split = line.split(":");
+                    if (Objects.equals(searchWord, split[0])) {
+                        String[] postings = split[1].replace('[', ' ').replace(']', ' ').split(",");
+                        for (String posting : postings) {
+                            int bookId = Integer.parseInt(posting.trim());
+                            FictionBook book = new FictionBook(documents[bookId - 1]);
+                            PostingWeight postingDocument = new PostingWeight(bookId, Scores.CONTENT);
+                            checkTitle(book, searchWord, postingDocument);
+                            checkAuthors(book, searchWord, postingDocument);
+                            result.add(postingDocument);
+                        }
+                        break;
                     }
                 }
+            } catch (IOException | ParserConfigurationException | SAXException e) {
+                e.printStackTrace();
             }
-        } catch (IOException | ParserConfigurationException | SAXException e) {
-            e.printStackTrace();
         }
-        return result;
+        System.out.println("Resulting list with weight: " + result);
+        Map<Integer, Double> resMap = sumThePostings(result);
+        return convertKeyToTitle(getMaxKey(resMap));
     }
 
     private void checkTitle(FictionBook book, String searchWord, PostingWeight postingDocument) {
@@ -57,20 +63,39 @@ public class PostingWeightSearcher {
 
     private void checkAuthors(FictionBook book, String searchWord, PostingWeight postingDocument) {
         ArrayList<Person> authorArr = book.getAuthors();
-        for (int j = 0; j < authorArr.size(); j++) {
-            Person author = authorArr.get(j);
+        for (Person author : authorArr) {
             if (author.getFullName().toLowerCase().contains(searchWord))
                 postingDocument.addZoneWeight(Scores.AUTHOR);
         }
     }
 
-//    private void checkDescription(FictionBook book, String searchWord, PostingWeight postingDocument) {
-//        Description bookDescription = book.getDescription();
-//        DocumentInfo docInfo = bookDescription.getDocumentInfo();
-//        docInfo.g
-//        for (String title : titleArr) {
-//            if (title.toLowerCase().equals(searchWord))
-//                postingDocument.addZoneWeight(Scores.TITLE);
-//        }
-//    }
+    private Map<Integer, Double> sumThePostings(List<PostingWeight> list) {
+        Map<Integer, Double> postingMap = new HashMap<>();
+        for (PostingWeight postingWeight : list) {
+            int currId = postingWeight.getId();
+            double currPostingWeight = postingWeight.getWeight();
+            if (postingMap.containsKey(currId)) {
+                double currWeight = postingMap.get(currId);
+                postingMap.put(currId, currPostingWeight + currWeight);
+            } else
+                postingMap.put(currId, currPostingWeight);
+        }
+        return postingMap;
+    }
+
+    private int getMaxKey(Map<Integer, Double> map) {
+        double maxVal = -1;
+        int maxKey = -1;
+        for (Map.Entry<Integer, Double> entry : map.entrySet()) {
+            if (entry.getValue() > maxVal) {
+                maxVal = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+        return maxKey;
+    }
+
+    private String convertKeyToTitle(int key) {
+        return documents[key-1].getName();
+    }
 }
